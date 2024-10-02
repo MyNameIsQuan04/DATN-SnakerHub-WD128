74b4e73a39a15323dae64c0cf97813790c6c3b81
Trusthy136 <nguyenphuongquan04@gmail.com>
2024-10-02 20:36:14 +0700

    Admin Dashboard
    Giao diện Admin

--- FE/src/components/layout/layoutClient/layoutClient.tsx
+++ FE/src/components/layout/layoutClient/layoutClient.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "../../header/header";
+import { Outlet } from "react-router-dom";
+import Footer from "../../footer/footer";
 
 const LayoutClient = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow z-0">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default LayoutClient;
---
+++ FE/src/pages/admin/ListColor.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListColor = () => {
+  return <div></div>;
+};
+
+export default ListColor;
