e5b7de563526d017e82b0542c85b3c3ff5afcefe
dangvu27 <128789582+dangvu27@users.noreply.github.com>
2024-11-14 12:33:19 +0700

    backcode

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: {
+      clientPort: 443,
+    },
+    host: '0.0.0.0',
+    cors: true,
+  },
 })
