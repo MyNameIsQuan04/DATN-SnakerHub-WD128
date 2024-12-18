7858bc8d8ca7c434638bc317af0ff7dafb08fc21
zuong123 <139326345+zuong123@users.noreply.github.com>
2024-12-18 21:23:39 +0700

    update voucher

--- BE/app/Http/Controllers/api/VoucherController.php
+++ BE/app/Http/Controllers/api/VoucherController.php
@@ -23,6 +23,7 @@ class VoucherController extends Controller
             'codeDiscount' => 'required|unique:vouchers,codeDiscount|max:10',
             'discount' => 'required|numeric|min:1|max:50',
             'type' => 'required|in:percent,amount',
+            'start_date' => 'required|date|after_or_equal:today',
             'expiration_date' => 'required|date',
             'usage_limit' => 'required|integer|min:1',
         ]);
@@ -49,6 +50,7 @@ class VoucherController extends Controller
             'type' => 'required|in:percent,amount',
             'expiration_date' => 'required|date',
             'usage_limit' => 'required|integer|min:1',
+            'start_date' => 'required|date|after_or_equal:today',
         ]);
 
         $voucher->update([
