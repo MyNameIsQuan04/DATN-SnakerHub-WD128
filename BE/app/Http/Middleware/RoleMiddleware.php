d31c257fa9a4feba1bd0a1b373d9a45aced2bead
zuong123 <139326345+zuong123@users.noreply.github.com>
2024-12-24 09:21:54 +0700

    roles

--- BE/app/Http/Middleware/RoleMiddleware.php
+++ BE/app/Http/Middleware/RoleMiddleware.php
@@ -13,13 +13,13 @@ class RoleMiddleware
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string  $role
+     * @param  mixed  ...$roles  Danh sách vai trò
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function handle(Request $request, Closure $next, string $role): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
         // Kiểm tra người dùng đã đăng nhập và vai trò của họ
-        if (auth()->check() && auth()->user()->role === $role) {
+        if (auth()->check() && in_array(auth()->user()->role, $roles)) {
             return $next($request);
         }
 
