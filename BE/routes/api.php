2622c366979619b5fa8514001437a1bdbe533a48
dangvu27 <128789582+dangvu27@users.noreply.github.com>
2025-01-03 21:43:36 +0700

    list history

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -5,24 +5,25 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\api\OrderController;
 use App\Http\Controllers\api\SlideController;
+use App\Http\Controllers\api\HistoryController;
 use App\Http\Controllers\api\ProductController;
 use App\Http\Controllers\api\SizeApiController;
 use App\Http\Controllers\api\UserApiController;
 use App\Http\Controllers\api\VoucherController;
 use App\Http\Controllers\Client\CartController;
-use App\Http\Controllers\Client\VoucherController as ClientVoucherController;
+use App\Http\Controllers\Client\SizeController;
 use App\Http\Controllers\api\CategoryController;
 use App\Http\Controllers\api\ColorApiController;
+use App\Http\Controllers\Client\ColorController;
 use App\Http\Controllers\api\DashboardController;
+use App\Http\Controllers\Client\ClientController;
 use App\Http\Controllers\Client\CommentController;
+use App\Http\Controllers\Client\CustomerController;
 use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 use App\Http\Controllers\Client\OrderController as ApiMemberOrderController;
 use App\Http\Controllers\Client\ProductController as ClientProductController;
+use App\Http\Controllers\Client\VoucherController as ClientVoucherController;
 use App\Http\Controllers\Client\CategoryControlller as ClientCategoryControlller;
-use App\Http\Controllers\Client\ClientController;
-use App\Http\Controllers\Client\ColorController;
-use App\Http\Controllers\Client\CustomerController;
-use App\Http\Controllers\Client\SizeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -62,8 +63,8 @@ foreach ($crud as $key => $controller) {
 
 Route::get('client/categories', [ClientCategoryControlller::class, 'index']);
 
-Route::get('dashboard/daily', [DashboardController::class, 'daily']);
-Route::get('dashboard/monthly', [DashboardController::class, 'monthly']);
+Route::post('dashboard/daily', [DashboardController::class, 'daily']);
+Route::post('dashboard/monthly', [DashboardController::class, 'monthly']);
 Route::get('dashboard', [DashboardController::class, 'index']);
 
 // Route::get('dashboard/daily', [DashboardController::class, 'daily'])->middleware('auth:api')->middleware('type:admin');
@@ -169,3 +170,6 @@ Route::prefix('client')->group(function () {
     Route::get('vouchers', [ClientVoucherController::class, 'index']);
     Route::get('vouchers/{id}', [ClientVoucherController::class, 'show']);
 });
+
+Route::get('/history', [HistoryController::class, 'index']);
+Route::post('/history-filter', [HistoryController::class, 'historyFilter']);
