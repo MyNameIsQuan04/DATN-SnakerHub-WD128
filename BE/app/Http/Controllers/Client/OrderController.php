c0110df2e9f5b6031bbc4b8d8773340f2adfa278
dothainam <namdtph44461@fpt.edu.vn>
2025-01-06 21:34:42 +0700

    fix

--- BE/app/Http/Controllers/Client/OrderController.php
+++ BE/app/Http/Controllers/Client/OrderController.php
@@ -297,10 +297,9 @@ class OrderController extends Controller
             ]);
 
 
-            $order->load('orderItems.productVariant.product', 'orderItems.productVariant.size', 'orderItems.productVariant.color', 'customer');
+            $order->load('orderItems', 'customer');
 
-
-            SendKhieuNaiOrderEmail::dispatch($order);
+            // SendKhieuNaiOrderEmail::dispatch($order);
             $order->orderItems->map(function ($orderItem) {
                 $orderItem->productVariantImage = Product_Variant::where('product_id', Product::where('name', $orderItem->nameProduct)->value('id'))
                     ->where('color_id', Color::where('name', $orderItem->color)->value('id'))
