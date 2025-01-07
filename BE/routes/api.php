440fcb91def8734f3cc215bdbc769c21c8f0f226
dothainam <namdtph44461@fpt.edu.vn>
2025-01-07 15:59:31 +0700

    fixfix

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -117,7 +117,7 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/client/user/{user}', [ClientController::class, 'show']);
 
     // Hiển thị danh sách người dùng (Admin chỉ có thể truy cập)
-    Route::get('/users', [UserApiController::class, 'index'])->middleware('role:admin');
+    Route::get('/users', [UserApiController::class, 'index'])->middleware('role:Admin');
 
     // Hiển thị thông tin người dùng (cho cả Admin và User)
     Route::get('/users/{id}', [UserApiController::class, 'show']);
