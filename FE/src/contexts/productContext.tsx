ed8f0aa4e165295289bb20f6ecfb5ad3cf1eb9ec
dothainam <namdtph44461@fpt.edu.vn>
2025-01-07 15:36:06 +0700

    fixfix

--- FE/src/contexts/productContext.tsx
+++ FE/src/contexts/productContext.tsx
@@ -81,7 +81,7 @@ const ProductContext = ({ children }: Props) => {
       );
       setProducts(newProductsAfterUpdate);
       router("/admin/product");
-      // window.location.reload();
+      window.location.reload();
     } catch (error) {
       console.log(error);
     } finally {
