318a6418a242242ad5965f9b68aa4214b437aec6
zuong123 <139326345+zuong123@users.noreply.github.com>
2025-01-10 14:47:56 +0700

    voucher

--- BE/app/Http/Controllers/api/CommentController.php
+++ BE/app/Http/Controllers/api/CommentController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Comment;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
